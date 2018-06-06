e693f35bfa41dc43ae4c6d7fc87b137d609bd60a
Eduard Surov <esurov@avito.ru>
2018-06-06 19:49:56 +0300

    Translation scheme work in progress

--- src/TranslationScheme.php
+++ src/TranslationScheme.php
@@ -10,8 +10,6 @@ use Remorhaz\UniLex\Parser\Symbol;
 class TranslationScheme implements TranslationSchemeInterface
 {
 
-    private $pathBufferList = [];
-
     private $varList = [];
 
     /**
@@ -44,18 +42,41 @@ class TranslationScheme implements TranslationSchemeInterface
         $hash = "{$production->getHeader()->getSymbolId()}.{$production->getIndex()}";
         switch ($hash) {
             case SymbolType::NT_JSON_PATH . ".0":
-                $pathBufferId = $symbols[0]['s.path_buffer_id'];
-                var_dump("RETURN BUFFER({$pathBufferId})");
+                $pathId = $symbols[0]['s.path_id'];
+                var_dump("RETURN VAR:{$pathId}");
                 break;
 
             case SymbolType::NT_PATH . ".0":
-                $header['s.path_buffer_id'] = $symbols[1]['i.path_buffer_id'];
+                $header['s.path_id'] = $symbols[1]['s.path_id'];
                 break;
 
             case SymbolType::NT_DOT_FILTER_NEXT . ".0":
-                $pathBufferId = $header['i.path_buffer_id'];
+                $pathId = $header['i.path_id'];
                 $function = $header['i.filter_name'];
-                var_dump("BUFFER({$pathBufferId}) = BUFFER({$pathBufferId}).{$function}()");
+                var_dump("// dataset aggregate function");
+                $functionVarId = $this->createVar();
+                var_dump("VAR:{$functionVarId} = STRING('{$function}')");
+                $varId = $this->createVar();
+                var_dump("VAR:{$varId} = VAR:{$pathId}.AGGREGATE(VAR:{$functionVarId})");
+                var_dump("VAR:{$pathId}.UNSET");
+                var_dump("VAR:{$functionVarId}.UNSET");
+                $header['s.path_id'] = $varId;
+                break;
+
+            case SymbolType::NT_DOT_FILTER_NEXT . ".1":
+                $header['s.path_id'] = $symbols[0]['s.path_id'];
+                break;
+
+            case SymbolType::NT_DOT_FILTER . ".0":
+                $header['s.path_id'] = $symbols[1]['s.path_id'];
+                break;
+
+            case SymbolType::NT_DOT_FILTER . ".1":
+                $header['s.path_id'] = $symbols[1]['s.path_id'];
+                break;
+
+            case SymbolType::NT_FILTER_LIST . ".0":
+                $header['s.path_id'] = $symbols[1]['s.path_id'];
                 break;
 
             case SymbolType::NT_EXPR_ARG_SCALAR . ".0":
@@ -63,18 +84,18 @@ class TranslationScheme implements TranslationSchemeInterface
                 break;
 
             case SymbolType::NT_EXPR_ARG_SCALAR . ".1":
-                $pathBufferId = $header['i.path_buffer_id'];
-                $inlinePathBufferId = $symbols[0]['s.path_buffer_id'];
+                $pathId = $header['i.path_id'];
+                $inlinePathBufferId = $symbols[0]['s.path_id'];
                 $varId = $this->createVar();
-                var_dump("BUFFER({$pathBufferId}).VAR({$varId}) <- BUFFER({$inlinePathBufferId})");
+                var_dump("VAR:{$varId} = VAR:{$inlinePathBufferId}.UNFORK(VAR:{$pathId})");
                 $header['s.var_id'] = $varId;
                 break;
 
             case SymbolType::NT_EXPR_ARG_SCALAR . ".2":
-                $pathBufferId = $header['i.path_buffer_id'];
+                $pathId = $header['i.path_id'];
                 $int = $symbols[0]['s.int'];
                 $varId = $this->createVar();
-                var_dump("BUFFER({$pathBufferId}).VAR({$varId}) <- INT({$int})");
+                var_dump("VAR:{$varId} = INT({$int}).MAP(VAR:{$pathId})");
                 $header['s.var_id'] = $varId;
                 break;
 
@@ -145,22 +166,34 @@ class TranslationScheme implements TranslationSchemeInterface
                 break;
 
             case SymbolType::NT_BRACKET_FILTER . ".4":
-                $pathBufferId = $header['i.path_buffer_id'];
+                $pathId = $header['i.path_id'];
                 $varId = $symbols[2]['s.var_id'];
-                var_dump("BUFFER({$pathBufferId}).FILTER_KEY BUFFER({$pathBufferId}).VAR({$varId})");
+                var_dump("// filter by key");
+                $newPathId = $this->createVar();
+                var_dump("VAR:{$newPathId} = VAR:{$pathId}.FILTER_KEY(VAR:{$varId})");
+                var_dump("VAR:{$pathId}.UNSET");
+                var_dump("VAR:{$varId}.UNSET");
+                $header['s.path_id'] = $newPathId;
                 break;
 
             case SymbolType::NT_BRACKET_FILTER . ".5":
-                $pathBufferId = $header['i.path_buffer_id'];
+                $pathId = $header['i.path_id'];
                 $varId = $symbols[3]['s.var_id'];
-                var_dump("BUFFER({$pathBufferId}).FILTER BUFFER({$pathBufferId}).VAR({$varId})");
+                var_dump("// filter by value");
+                $newPathId = $this->createVar();
+                var_dump("VAR:{$newPathId} = VAR:{$pathId}.FILTER(VAR:{$varId})");
+                var_dump("VAR:{$pathId}.UNSET");
+                var_dump("VAR:{$varId}.UNSET");
+                $header['s.path_id'] = $newPathId;
                 break;
 
             case SymbolType::NT_EXPR_ARG_COMP . ".0":
-                $pathBufferId = $header['i.path_buffer_id'];
                 $varId = $symbols[1]['s.var_id'];
-                var_dump("BUFFER({$pathBufferId}).VAR({$varId}) = NOT(VAR({$varId}))");
-                $header['s.var_id'] = $varId;
+                var_dump("// NOT expression");
+                $newVarId = $this->createVar();
+                var_dump("VAR:{$newVarId} = NOT(VAR:{$varId})");
+                var_dump("VAR:{$varId}.UNSET");
+                $header['s.var_id'] = $newVarId;
                 break;
 
             case SymbolType::NT_EXPR_ARG_COMP . ".1":
@@ -170,8 +203,11 @@ class TranslationScheme implements TranslationSchemeInterface
             case SymbolType::NT_EXPR_ARG_COMP_TAIL . ".0":
                 $leftVarId = $header['i.var_id'];
                 $rightVarId = $symbols[2]['s.var_id'];
+                var_dump("// EQ expression");
                 $varId = $this->createVar();
-                var_dump("VAR({$varId}) = VAR({$leftVarId}) EQ VAR({$rightVarId})");
+                var_dump("VAR:{$varId} = EQ(VAR:{$leftVarId}, VAR:{$rightVarId})");
+                var_dump("VAR:{$leftVarId}.UNSET");
+                var_dump("VAR:{$rightVarId}.UNSET");
                 $header['s.var_id'] = $varId;
                 break;
 
@@ -186,8 +222,11 @@ class TranslationScheme implements TranslationSchemeInterface
             case SymbolType::NT_EXPR_ARG_AND_TAIL . ".0":
                 $leftVarId = $header['i.var_id'];
                 $rightVarId = $symbols[2]['s.var_id'];
+                var_dump("// AND expression");
                 $varId = $this->createVar();
-                var_dump("VAR({$varId}) = VAR({$leftVarId}) AND VAR({$rightVarId})");
+                var_dump("VAR:{$varId} = AND(VAR:{$leftVarId}, VAR:{$rightVarId})");
+                var_dump("VAR:{$leftVarId}.UNSET");
+                var_dump("VAR:{$rightVarId}.UNSET");
                 $header['s.var_id'] = $varId;
                 break;
 
@@ -202,8 +241,11 @@ class TranslationScheme implements TranslationSchemeInterface
             case SymbolType::NT_EXPR_ARG_OR_TAIL . ".0":
                 $leftVarId = $header['i.var_id'];
                 $rightVarId = $symbols[2]['s.var_id'];
+                var_dump("// OR expression");
                 $varId = $this->createVar();
-                var_dump("VAR({$varId}) = VAR({$leftVarId}) OR VAR({$rightVarId})");
+                var_dump("VAR:{$varId} = OR(VAR:{$leftVarId}, VAR:{$rightVarId})");
+                var_dump("VAR:{$leftVarId}.UNSET");
+                var_dump("VAR:{$rightVarId}.UNSET");
                 $header['s.var_id'] = $varId;
                 break;
 
@@ -240,78 +282,81 @@ class TranslationScheme implements TranslationSchemeInterface
                 $rootName = $symbols[0]['s.text'];
                 $isInlinePath = $header['i.is_inline_path'];
                 $pathType = $this->getPathRootType($rootName, $isInlinePath);
-                $pathBufferId = $this->createPathBuffer();
+                $pathId = $this->createVar();
                 if ($isInlinePath) {
-                    $parentPathBufferId = $header['i.path_buffer_id'];
-                    var_dump("BUFFER({$pathBufferId}) <- BUFFER({$parentPathBufferId})");
+                    $parentPathId = $header['i.path_id'];
+                    var_dump("// init inline path");
+                    var_dump("VAR:{$pathId} = VAR:{$parentPathId}.FORK");
                 } else {
-                    var_dump("BUFFER({$pathBufferId}) <- @ROOT");
+                    var_dump("// init root path");
+                    var_dump("VAR:{$pathId} = DATASET(@ROOT)");
                 }
-                $symbols[1]['i.path_buffer_id'] = $pathBufferId;
+                $symbols[1]['i.path_id'] = $pathId;
                 $symbols[1]['i.path_type'] = $pathType;
                 $symbols[1]['i.is_inline_path'] = $isInlinePath;
                 break;
 
             case SymbolType::NT_BRACKET_FILTER . ".1.0":
-                $pathBufferId = $header['i.path_buffer_id'];
-                var_dump("BUFFER({$pathBufferId}) = BUFFER({$pathBufferId}).PROPERTY // [string list]");
+                $pathId = $header['i.path_id'];
+                var_dump("// [string list]");
+                var_dump("VAR:{$pathId} = VAR:{$pathId}.NEXT_KEY");
                 break;
 
             case SymbolType::NT_BRACKET_FILTER . ".4.2":
-                $symbols[2]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[2]['i.path_id'] = $header['i.path_id'];
                 break;
 
             case SymbolType::NT_BRACKET_FILTER . ".5.3":
-                $symbols[3]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[3]['i.path_id'] = $header['i.path_id'];
                 break;
 
             case SymbolType::NT_EXPR . ".0.0":
-                $symbols[0]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[0]['i.path_id'] = $header['i.path_id'];
                 break;
 
             case SymbolType::NT_EXPR . ".0.1":
-                $symbols[1]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[1]['i.path_id'] = $header['i.path_id'];
                 $symbols[1]['i.var_id'] = $symbols[0]['s.var_id'];
                 break;
 
             case SymbolType::NT_EXPR_ARG_OR . ".0.0":
-                $symbols[0]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[0]['i.path_id'] = $header['i.path_id'];
                 break;
 
             case SymbolType::NT_EXPR_ARG_OR . ".0.1":
-                $symbols[1]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[1]['i.path_id'] = $header['i.path_id'];
                 $symbols[1]['i.var_id'] = $symbols[0]['s.var_id'];
                 break;
 
             case SymbolType::NT_EXPR_ARG_OR_TAIL . ".0.2":
-                $symbols[2]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[2]['i.path_id'] = $header['i.path_id'];
                 break;
 
             case SymbolType::NT_EXPR_ARG_AND . ".0.0":
-                $symbols[0]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[0]['i.path_id'] = $header['i.path_id'];
                 break;
 
             case SymbolType::NT_EXPR_ARG_AND . ".0.1":
-                $symbols[1]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[1]['i.path_id'] = $header['i.path_id'];
                 $symbols[1]['i.var_id'] = $symbols[0]['s.var_id'];
                 break;
 
             case SymbolType::NT_EXPR_ARG_AND_TAIL . ".0.2":
-                $symbols[2]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[2]['i.path_id'] = $header['i.path_id'];
                 break;
 
             case SymbolType::NT_EXPR_ARG_COMP . ".0.1":
-                $symbols[1]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[1]['i.path_id'] = $header['i.path_id'];
                 break;
 
             case SymbolType::NT_EXPR_ARG_COMP . ".1.0":
-                $symbols[0]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[0]['i.path_id'] = $header['i.path_id'];
                 break;
 
             case SymbolType::NT_EXPR_ARG_SCALAR . ".0.0":
             case SymbolType::NT_EXPR_ARG_SCALAR . ".2.0":
             case SymbolType::NT_EXPR_ARG_SCALAR . ".3.0":
-                $symbols[0]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[0]['i.path_id'] = $header['i.path_id'];
                 break;
 
             case SymbolType::NT_EXPR_ARG_COMP_TAIL . ".0.2":
@@ -322,11 +367,11 @@ class TranslationScheme implements TranslationSchemeInterface
             case SymbolType::NT_EXPR_ARG_COMP_TAIL . ".5.2":
             case SymbolType::NT_EXPR_ARG_COMP_TAIL . ".6.2":
             case SymbolType::NT_EXPR_ARG_COMP_TAIL . ".7.2":
-                $symbols[2]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[2]['i.path_id'] = $header['i.path_id'];
                 break;
 
             case SymbolType::NT_EXPR_GROUP . ".0.2":
-                $symbols[2]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[2]['i.path_id'] = $header['i.path_id'];
                 break;
 
             case SymbolType::NT_STRING_LIST . ".0.2":
@@ -340,39 +385,45 @@ class TranslationScheme implements TranslationSchemeInterface
                 break;
 
             case SymbolType::NT_FILTER_LIST . ".0.1":
-                $symbols[1]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[1]['i.path_id'] = $header['i.path_id'];
                 break;
 
             case SymbolType::NT_FILTER_LIST . ".1.2":
-                $symbols[2]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[2]['i.path_id'] = $header['i.path_id'];
                 break;
 
             case SymbolType::NT_FILTER_LIST . ".2.2":
-                $symbols[2]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[2]['i.path_id'] = $header['i.path_id'];
                 break;
 
             case SymbolType::NT_FILTER_LIST . ".2.4":
-                $symbols[4]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[4]['i.path_id'] = $symbols[2]['s.path_id'];
                 break;
 
             case SymbolType::NT_DOT_FILTER . ".0.1":
-                $symbols[1]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[1]['i.path_id'] = $header['i.path_id'];
                 $symbols[1]['i.filter_name'] = $symbols[0]['s.text'];
                 break;
 
             case SymbolType::NT_DOT_FILTER . ".1.1":
-                $symbols[1]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[1]['i.path_id'] = $header['i.path_id'];
                 break;
 
             case SymbolType::NT_DOT_FILTER_NEXT . ".1.0":
                 $filterName = $header['i.filter_name'];
-                $pathBufferId = $header['i.path_buffer_id'];
-                var_dump("BUFFER({$pathBufferId}) = BUFFER({$pathBufferId}).NEXT_KEY '{$filterName}' // .name");
-                $symbols[0]['i.path_buffer_id'] = $pathBufferId;
+                $pathId = $header['i.path_id'];
+                $varId = $this->createVar();
+                var_dump("// .name");
+                var_dump("VAR:{$varId} = STRING('{$filterName}')");
+                $newPathId = $this->createVar();
+                var_dump("VAR:{$newPathId} = VAR:{$pathId}.NEXT_KEY(VAR:{$varId})");
+                var_dump("VAR:{$pathId}.UNSET");
+                var_dump("VAR:{$varId}.UNSET");
+                $symbols[0]['i.path_id'] = $newPathId;
                 break;
 
             case SymbolType::NT_EXPR_ARG_SCALAR . ".1.0":
-                $symbols[0]['i.path_buffer_id'] = $header['i.path_buffer_id'];
+                $symbols[0]['i.path_id'] = $header['i.path_id'];
                 $symbols[0]['i.is_inline_path'] = true;
                 break;
 
@@ -410,13 +461,6 @@ class TranslationScheme implements TranslationSchemeInterface
                 throw new Exception("Relative paths are allowed only in inline filters");
         }
         throw new Exception("Invalid path root: {$name}");
-    }
-
-    private function createPathBuffer(): int
-    {
-        $id = count($this->pathBufferList);
-        $this->pathBufferList[$id] = [];
-        return $id;
     }
 
     private function createVar(): int
