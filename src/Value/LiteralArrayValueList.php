1131f071fe9d01819091967382c9f6755c7b397e
remorhaz <zoohie@gmail.com>
2019-07-28 13:19:20 +0300

    Literal array value improved

--- src/Value/LiteralArrayValueList.php
+++ src/Value/LiteralArrayValueList.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Remorhaz\JSON\Path\Value;
 
 use function array_fill_keys;
+use function array_map;
 use Remorhaz\JSON\Data\Value\ValueInterface;
 
 final class LiteralArrayValueList implements ValueListInterface
@@ -11,24 +12,19 @@ final class LiteralArrayValueList implements ValueListInterface
 
     private $indexMap;
 
-    private $values = [];
+    private $values;
+
+    private $valueLists;
 
     public function __construct(IndexMapInterface $indexMap, ValueListInterface ...$valueLists)
     {
         $this->indexMap = $indexMap;
-        $elementLists = array_fill_keys($this->indexMap->getInnerIndice(), []);
-        foreach ($valueLists as $listIndex => $valueList) {
+        foreach ($valueLists as $valueList) {
             if (!$this->indexMap->equals($valueList->getIndexMap())) {
                 throw new Exception\IndexMapMatchFailedException($valueList, $this);
             }
-
-            foreach ($valueList->getValues() as $valueIndex => $value) {
-                $elementLists[$valueIndex][] = $value;
-            }
         }
-        foreach ($elementLists as $index => $elements) {
-            $this->values[$index] = new LiteralArrayValue($this->indexMap, ...$elements);
-        }
+        $this->valueLists = $valueLists;
     }
 
     public function getIndexMap(): IndexMapInterface
@@ -38,15 +34,37 @@ final class LiteralArrayValueList implements ValueListInterface
 
     public function getValues(): array
     {
+        if (!isset($this->values)) {
+            $this->values = $this->loadValues();
+        }
+
         return $this->values;
+    }
+
+    private function loadValues(): array
+    {
+        $elementLists = array_fill_keys($this->indexMap->getInnerIndice(), []);
+        foreach ($this->valueLists as $valueList) {
+            foreach ($valueList->getValues() as $innerIndex => $value) {
+                $elementLists[$innerIndex][] = $value;
+            }
+        }
+
+        return array_map([$this, 'createValue'], $elementLists);
+    }
+
+    private function createValue(array $elements): ValueInterface
+    {
+        return new LiteralArrayValue($this->indexMap, ...$elements);
     }
 
     public function getValue(int $index): ValueInterface
     {
-        if (!isset($this->values[$index])) {
+        $values = $this->getValues();
+        if (!isset($values[$index])) {
             throw new Exception\ValueNotFoundException($index, $this);
         }
 
-        return $this->values[$index];
+        return $values[$index];
     }
 }
