a4b23dc99c63f96eb446a4ff2ca3ca23e2e33c65
remorhaz <zoohie@gmail.com>
2021-04-09 18:29:44 +0300

    Memory leak test added

---
+++ tests/MemoryLeakTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Remorhaz\JSON\Path\Test;
+
+use PHPUnit\Framework\TestCase;
+use Remorhaz\JSON\Path\Query\QueryFactory;
+
+use function array_fill;
+use function array_unique;
+use function memory_get_usage;
+
+/**
+ * @coversNothing
+ */
+class MemoryLeakTest extends TestCase
+{
+
+    public function testQueryCompilation(): void
+    {
+        $queryFactory = QueryFactory::create();
+        $attemptCount = 100;
+        $memory = array_fill(0, $attemptCount, 0);
+        for ($i = 0; $i < $attemptCount; $i++) {
+            $queryFactory
+                ->createQuery('$.a.b[?(@.c==1)].d')
+                ->getCapabilities();
+            $memory[$i] = memory_get_usage();
+        }
+        self::assertCount(1, array_unique($memory));
+    }
+}
